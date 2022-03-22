bb5c1986679aba5df465c6e700a327b3dea2f9ca
KuenJane <100176003+KuenJane@users.noreply.github.com>
2022-03-22 12:01:22 +0800

    完成角色控制器/添加武器資料

---
+++ 014/Assets/Scripts/DetaWeapon.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+
+namespace Jane 
+{
+    [CreateAssetMenu(menuName ="Jane/Deta weapon",fileName = "Deta Weapon")]
+    public class DetaWeapon : ScriptableObject
+    {
+        
+        
+    }
+
+}
+
